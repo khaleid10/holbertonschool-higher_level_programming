418b8eb80cca57f54cf6982766772f830723224d
khaleid10 <khaledaloraini@outlook.com>
2026-06-14 17:17:24 +0300

    Add add_integer function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """Add two integers."""
+"""Module for add_integer function."""
 
+
+def add_integer(a, b=98):
+    """Adds two integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
