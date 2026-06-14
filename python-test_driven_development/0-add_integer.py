32cb8115ad363bf04f2475e6d107304d7d06a876
khaleid10 <khaledaloraini@outlook.com>
2026-06-14 17:32:11 +0300

    Update add_integer tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module for add_integer function."""
+"""Module containing add_integer."""
 
 
 def add_integer(a, b=98):
-    """Adds two integers."""
+    """Add two integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
